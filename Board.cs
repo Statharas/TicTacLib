803386c42b9e0c65c8dfc83f200d070286e5461e
Stathis Papadopoulos <stathisthemagic@gmail.com>
2021-01-31 00:54:37 +0200

    Initial Commit

---
+++ Board.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TicTac
+{
+    public static partial class Board
+    {
+        [Flags]
+        public enum State
+        {
+            Empty = 0,
+            P1 = 1,
+            P2 = 2,
+            Full=3
+        }
+        private static State[,] Pattern = new State[3,3];
+        /// <summary>
+        /// Resets the board state. Use this when you want to restart the game.
+        /// </summary>
+        public static void Clear(){
+            Pattern = new State[3,3] {
+                { State.Empty, State.Empty, State.Empty },
+                { State.Empty, State.Empty, State.Empty }, 
+                { State.Empty, State.Empty, State.Empty }};
+        }
+        /// <summary>
+        /// Assigns a player move to the board. This is player agnostic, but we record it on the playlist.
+        /// </summary>
+        /// <param name="Check">The change of state pushed by a player or AI</param>
+        /// <returns>Returns true if it succeeds, false if it's an illegal move</returns>
+        public static bool Assign(State Check, ushort Row, ushort Column){
+            if(Check==State.Empty||Check==State.Full){
+                return false;
+            }
+            if (Pattern[Row, Column] != Board.State.Empty)
+                return false;
+            Pattern[Row, Column] = Check;
+            return true;
+        }
+        /// <summary>
+        /// Gets the boardstate, which is player agnostic. Use this to mask which options are unavailable.
+        /// </summary>
+        /// <returns>Returns the board state</returns>
+        public static State[,] GetBoard(){
+            return Pattern;
+        }
+        public static void SetBoard(State[,] q){
+            Pattern = q;
+        }
+        
+    }
+}
---
+++ ΑΙ.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TicTac.Board;
+
+namespace TicTac
+{
+    public static partial class ΑΙ
+    {
+        public static ushort[] Play()
+        {
+
+            return new ushort[2] { 1, 1 };
+        }
+        public static State CheckForWins(Board.State[,] Pattern)
+        {
+            State z = State.P1;
+            for (int x = 0; x < 2; x++)
+            {
+                if (x == 1)
+                    z = State.P2;
+                if (//Horizontal check
+                       (Pattern[0, 0] == z && Pattern[0, 1] == z && Pattern[0, 2] == z)
+                    || (Pattern[1, 0] == z && Pattern[1, 1] == z && Pattern[1, 2] == z)
+                    || (Pattern[2, 0] == z && Pattern[2, 1] == z && Pattern[2, 2] == z)
+                    //Vertical Check
+                    || (Pattern[0, 0] == z && Pattern[1, 0] == z && Pattern[2, 0] == z)
+                    || (Pattern[0, 1] == z && Pattern[1, 1] == z && Pattern[2, 1] == z)
+                    || (Pattern[0, 2] == z && Pattern[1, 2] == z && Pattern[2, 2] == z)
+                    //Cross Check
+                    || (Pattern[0, 0] == z && Pattern[1, 1] == z && Pattern[2, 2] == z)
+                    || (Pattern[2, 0] == z && Pattern[1, 1] == z && Pattern[0, 2] == z)
+                    )
+                    return z;
+            }
+            //if there's any blank place, return empty (as in, there's at least 1 empty slot
+            foreach (State q in Pattern)
+            {
+                if (q == State.Empty)
+                    return State.Empty;
+            }
+            //If we reach this point, it means that the board doesn't have any slots left. We should tell this to the front end.
+            return State.Full;
+        }
+    }
+}
